eebdd1b4b38d71f5940b9952f18b55b3443336de
DayceeLau <97490603+DayceeLau@users.noreply.github.com>
2022-03-27 23:44:41 +0800

    Update CheckingAccount.java

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,39 +1,39 @@
-public class Wk08CheckingAccount
+public class CheckingAccount
 {
     private Double balance;
     private int accountNumber;
 
-    Wk08CheckingAccount(int accountNumber)
+    WCheckingAccount(int accountNumber)
     {
         this.accountNumber = accountNumber;
         this.balance = 0.0;
     }
 
-    Wk08CheckingAccount(int accountNumber, Double balance)
+    CheckingAccount(int accountNumber, Double balance)
     {
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
 
-    public void deposit(double deposit) throws Wk08InsufficientFundsException
+    public void deposit(double deposit) throws InsufficientFundsException
     {
-        if( deposit>=0 )  this.balance += deposit;
-        if(deposit <= 0){
-            throw new Wk08InsufficientFundsException("No deposit of negative numbers or 0.");
+        if( deposit>=0 )  this.balance += deposit;      //If deposit amount > 0 do function
+        if(deposit <= 0){                               //If deposit amount < 0 throw exception
+            throw new InsufficientFundsException("No deposit of negative numbers or 0.");
         }
     }
 
-    public void withdraw(double withdrawal) throws Wk08InsufficientFundsException
+    public void withdraw(double withdrawal) throws InsufficientFundsException
     {
-        if(withdrawal >=0 )
+        if(withdrawal >=0 )     //If withdrawal amount > 0
         {
             if(withdrawal > this.balance)
             {
-                throw new Wk08InsufficientFundsException("Withdrawal amount exceeds current balance.");
+                throw new InsufficientFundsException("Withdrawal amount exceeds current balance.");     //Throw exception if amount > balance
             }
             else
             {
-                this.balance -= withdrawal;
+                this.balance -= withdrawal;     //If amount< balance, do function
             }
         }
     }
